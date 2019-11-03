3c60a792579c0bad553e615667bd1e2d73072668
Benjamin Wang <wlongxiang1119@gmail.com>
2019-11-03 23:39:12 +0100

    Implement MLLP with numpy v0

--- assignment_1/code/mlp_numpy.py
+++ assignment_1/code/mlp_numpy.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from modules import * 
+from modules import *
 
 class MLP(object):
   """
@@ -37,7 +37,21 @@ class MLP(object):
     ########################
     # PUT YOUR CODE HERE  #
     #######################
-    raise NotImplementedError
+    self.n_inputs = n_inputs
+    self.n_hidden = n_hidden
+    self.n_classes = n_classes
+    self.neg_slope = neg_slope
+    self.layers = []
+    n_neurons = [self.n_inputs] + self.n_hidden + [self.n_classes]
+    for i in range(len(n_neurons) - 1):
+        linear_module = LinearModule(in_features=n_neurons[i], out_features=n_neurons[i+1])
+        self.layers.append(linear_module)
+        # note that we do not need relu module for the last layer (output), we apply softmax later
+        if i < len(n_neurons) - 2:
+          leakeyrelu_modulle = LeakyReLUModule(neg_slope=self.neg_slope)
+          self.layers.append(leakeyrelu_modulle)
+    softmax_module = SoftMaxModule()
+    self.layers.append(softmax_module)
     ########################
     # END OF YOUR CODE    #
     #######################
@@ -59,7 +73,9 @@ class MLP(object):
     ########################
     # PUT YOUR CODE HERE  #
     #######################
-    raise NotImplementedError
+    for layer in self.layers:
+      x = layer.forward(x)
+    out = x
     ########################
     # END OF YOUR CODE    #
     #######################
@@ -80,9 +96,20 @@ class MLP(object):
     ########################
     # PUT YOUR CODE HERE  #
     #######################
-    raise NotImplementedError
+    for layer in reversed(self.layers):
+      dout = layer.backward(dout)
     ########################
     # END OF YOUR CODE    #
     #######################
 
     return
+
+if __name__ == '__main__':
+    n_inputs = 6
+    n_hidden = [3,4,5]
+    n_classes = 3
+    neg_slope = 0.1
+    mlp = MLP(n_inputs, n_hidden, n_classes, neg_slope)
+    x = np.random.randn(7, 6)
+    mlp.forward(x)
+    mlp.backward(np.random.normal(size=(7,3)))
