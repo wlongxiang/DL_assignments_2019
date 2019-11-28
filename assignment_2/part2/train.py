177018abaabc95bb16ce471e65954cb105836d72
Benjamin Wang <wlongxiang1119@gmail.com>
2019-11-28 19:17:29 +0100

    Add docstrings

--- assignment_2/part2/train.py
+++ assignment_2/part2/train.py
@@ -33,62 +33,62 @@ from torch.utils.data import DataLoader
 from part2.dataset import TextDataset
 from part2.model import TextGenerationModel
 
+
 ################################################################################
 def predict(dataset, seq_length, model, device, custom_init_seq=None, tau=0):
+    """
+    Run prediction for LSTM model.
+
+    :param Dataset dataset: the pytorch dataset object as in TextDataset implementation.
+    :param int seq_length: the desired total sequence length for the final output of prediction
+    :param model nn.Module: a pytorch model inherit from nn.Module API
+    :param str device: weather to run this on "cpu" or "CUDA:0"
+    :param str custom_init_seq: initial sequqence of seeds, such as "sleeping beauty", default to None where one random chara
+        cter from corpus will be selected as seed
+    :param float tau: the random sampling temperature. Default to 0, corresponding to greedy sampling.
+    :return str: a generated sentense
+    """
     if custom_init_seq:
-        letters = []
+        init_sequence = []
         for char in custom_init_seq:
-            letter = dataset._char_to_ix[char]
-            letters.append(letter)
-        letters = torch.tensor(letters).reshape([1, len(custom_init_seq)]).to(device)
+            char_idx = dataset._char_to_ix[char]
+            init_sequence.append(char_idx)
+        init_sequence = torch.tensor(init_sequence).reshape([1, len(custom_init_seq)]).to(device)
     else:
-        letter = dataset._char_to_ix[np.random.choice(dataset._chars)]
-        letters = torch.tensor(letter).reshape([1, 1]).to(device)
+        # if the init sequence is not specified we choose an random one from the corpus
+        char_idx = dataset._char_to_ix[np.random.choice(dataset._chars)]
+        init_sequence = torch.tensor(char_idx).reshape([1, 1]).to(device)
 
-
-    for i in range(seq_length-letters.shape[1]):
-        raw_pred_letter_last = model.forward(letters)[:, :, -1] # shape (1, vocab_size)
+    for i in range(seq_length - init_sequence.shape[1]):
+        raw_pred_letter_last = model.forward(init_sequence)[:, :, -1]  # shape (1, vocab_size)
         if tau == 0:
-            predcited_letter = raw_pred_letter_last.argmax(dim=1).reshape(1,-1)
+            predcited_letter = raw_pred_letter_last.argmax(dim=1).reshape(1, -1)
         else:
-            letter_prob = torch.softmax(raw_pred_letter_last/tau, dim=1)
+            letter_prob = torch.softmax(raw_pred_letter_last / tau, dim=1)
             predcited_letter = torch.multinomial(letter_prob, 1).reshape([1, -1])
-        letters = torch.cat((letters, predcited_letter),1)
-    sentence = dataset.convert_to_string(letters.squeeze().numpy())
+        init_sequence = torch.cat((init_sequence, predcited_letter), 1)
+    sentence = dataset.convert_to_string(init_sequence.squeeze().numpy())
     return sentence
 
-def get_accuracy(predictions, targets):
+
+def calc_accuracy(predictions, targets):
     """
-    Computes the prediction accuracy, i.e. the average of correct predictions
-    of the network.
+    Calculate the training/test accuracy. Note that predictions is a tensor from one hot encoded result,
+    the max of dim of vocab_size is required to before comparing with targets which has the label for each time step.
+    After max operation, we just need to get number of correct predictions divided by all sequences in all batch.
 
-    Args:
-      predictions: 2D float array of size [batch_size, n_classes]
-      labels: 2D int array of size [batch_size, n_classes]
-              with one-hot encoding. Ground truth labels for
-              each sample in the batch
-    Returns:
-      accuracy: scalar float, the accuracy of predictions,
-                i.e. the average correct predictions over the whole batch
-
-    TODO:
-    Implement accuracy computation.
+    :param Tensor predictions: tensor of shape (batch_size, vocab_size, seq_length)
+    :param Tensor targets: tensor of shape (batch_size, seq_length)
+    :return float: accuracy
     """
-
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    batch_size,sqe_length = targets.shape
+    batch_size, sqe_length = targets.shape
+    # dimension in vocab_size dimension to squeeze the one hot encoded results
     _, y_pred = predictions.max(dim=1)
-    accuracy = (y_pred == targets).sum().item() / (batch_size * sqe_length)
-    ########################
-    # END OF YOUR CODE    #
-    #######################
-
+    accuracy = torch.sum(y_pred == targets).item() / (batch_size * sqe_length)
     return accuracy
 
-def train(config):
 
+def train(config):
     # Initialize the device which to run the model on
     device = torch.device(config.device)
     # Initialize the dataset and data loader (note the +1)
@@ -100,18 +100,18 @@ def train(config):
                                 # note that voccab size is number of characters, not words
                                 vocabulary_size=dataset.vocab_size,
                                 lstm_num_hidden=config.lstm_num_hidden,
-                                lstm_num_layers=config.lstm_num_layers, device=config.device).to(device) # fixme
+                                lstm_num_layers=config.lstm_num_layers, device=config.device).to(device)  # fixme
     # Setup the loss and optimizer
     criterion = nn.CrossEntropyLoss()  # fixme
     # optimizer = optim.RMSprop(model.parameters(), config.learning_rate)  # fixme
     optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)  # fixme
     # init csv file
     cvs_file = 'results/textgen_tau_{}_inputlength_{}_hiddenunits_{}_lr_{}_batchsize_{}_{}.csv'.format(config.tau,
-                                                                                                config.seq_length,
-                                                                                                config.lstm_num_hidden,
-                                                                                                 config.learning_rate,
-                                                                                                 config.batch_size,
-                                                                                                 int(time.time()))
+                                                                                                       config.seq_length,
+                                                                                                       config.lstm_num_hidden,
+                                                                                                       config.learning_rate,
+                                                                                                       config.batch_size,
+                                                                                                       int(time.time()))
     cols_data = ['step', 'train_loss', 'train_accuracy']
     with open(cvs_file, 'a') as fd:
         writer = csv.writer(fd)
@@ -135,20 +135,19 @@ def train(config):
         # clip the norm to avoid exploding gradients
         torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=config.max_norm)
 
-        loss = batch_loss.item()   # fixme
-        accuracy = get_accuracy(outputs, batch_targets)  # fixme
+        loss = batch_loss.item()  # fixme
+        accuracy = calc_accuracy(outputs, batch_targets)  # fixme
 
         # Just for time measurement
         t2 = time.time()
-        examples_per_second = config.batch_size/float(t2-t1)
+        examples_per_second = config.batch_size / float(t2 - t1)
 
         if step % config.print_every == 0:
-
             print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
                   "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                    config.train_steps, config.batch_size, examples_per_second,
-                    accuracy, loss
+                datetime.now().strftime("%Y-%m-%d %H:%M"), step,
+                config.train_steps, config.batch_size, examples_per_second,
+                accuracy, loss
             ))
             csv_data = [step, loss, accuracy]
             with open(cvs_file, 'a') as fd:
@@ -162,7 +161,8 @@ def train(config):
         if step % config.sample_every == 0:
             # Generate some sentences by sampling from the model
             for i in range(1):
-                sentense = predict(dataset, config.seq_length, model, device, custom_init_seq="sleeping beauty is ", tau=config.tau)
+                sentense = predict(dataset, config.seq_length, model, device, custom_init_seq="sleeping beauty is ",
+                                   tau=config.tau)
                 print(sentense)
                 with open(text_gen, 'a') as fp:
                     fp.write('{}:{}\n'.format(int(step), sentense))
@@ -177,16 +177,16 @@ def train(config):
     print('Done training.')
 
 
- ################################################################################
- ################################################################################
+################################################################################
+################################################################################
 
 if __name__ == "__main__":
-
     # Parse training configuration
     parser = argparse.ArgumentParser()
 
     # Model params
-    parser.add_argument('--txt_file', type=str, default="./assets/book_EN_grimms_fairy_tails.txt", help="Path to a .txt file to train on")
+    parser.add_argument('--txt_file', type=str, default="./assets/book_EN_grimms_fairy_tails.txt",
+                        help="Path to a .txt file to train on")
     parser.add_argument('--seq_length', type=int, default=40, help='Length of an input sequence')
     parser.add_argument('--lstm_num_hidden', type=int, default=128, help='Number of hidden units in the LSTM')
     parser.add_argument('--lstm_num_layers', type=int, default=2, help='Number of LSTM layers in the model')
